ed58b739df3daa958b6a144f10f1ac6f01e8c0ed
Виталя <egoshinvit@gmail.com>
2024-12-07 18:00:15 +0700

    hw6

--- src/s2-homeworks/hw05/header/Header.tsx
+++ src/s2-homeworks/hw05/header/Header.tsx
@@ -5,10 +5,11 @@ import {useLocation} from 'react-router-dom'
 import {PATH} from '../Pages'
 
 type PropsType = {
+    open: boolean
     handleOpen: () => void
 }
 
-export const Header: FC<PropsType> = ({handleOpen}) => {
+export const Header: FC<PropsType> = ({open, handleOpen}) => {
     // hw5-menu изначально отсутствует, при нажатии на бургер - появляется, при повторном нажатии исчезает
     const location = useLocation()
     const currentPath = location.pathname
@@ -24,13 +25,13 @@ export const Header: FC<PropsType> = ({handleOpen}) => {
     return (
         <>
             <div id={'hw5-header'} className={s.header}>
-                <img
+                {!open && <img
                     src={burgerIcon}
                     id={'hw5-burger-menu'}
                     className={s.burgerMenuIcon}
                     onClick={handleOpen}
                     alt={'open menu'}
-                />
+                />}
                 <h1>{pageName}</h1>
             </div>
         </>
