fbd6c4db179531b1833c3e01fdc70a48ae40c1b0
Виталя <egoshinvit@gmail.com>
2024-12-21 21:53:52 +0700

    hw8 design

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -8,16 +8,16 @@ export const homeWorkReducer = (state: UserType[], action: ActionType): UserType
     switch (action.type) {
         case 'sort': { // by name
             const compareFn = action.payload === 'up' ? (a: UserType, b: UserType) => {
-                if (a.name[0] < b.name[0]) {
+                if (a.name < b.name) {
                     return -1
-                } else if (a.name[0] > b.name[0]) {
+                } else if (a.name > b.name) {
                     return 1
                 }
                 return 0
             } : (a: UserType, b: UserType) => {
-                if (a.name[0] > b.name[0]) {
+                if (a.name > b.name) {
                     return -1
-                } else if (a.name[0] < b.name[0]) {
+                } else if (a.name < b.name) {
                     return 1
                 }
                 return 0
@@ -25,8 +25,14 @@ export const homeWorkReducer = (state: UserType[], action: ActionType): UserType
             return state.sort(compareFn)
         }
         case 'check': {
-
-            return state.filter(u => u.age >= action.payload)
+            return state.filter(u => u.age >= action.payload).sort((a: UserType, b: UserType) => {
+                if (a.age > b.age) {
+                    return -1
+                } else if (a.age < b.age) {
+                    return 1
+                }
+                return 0
+            })
         }
         default:
             return state
