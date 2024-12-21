fd5c22713e5eabae09287148d50532ade4f018a6
Виталя <egoshinvit@gmail.com>
2024-12-21 21:40:25 +0700

    hw8

--- src/s2-homeworks/hw08/HW8.tsx
+++ src/s2-homeworks/hw08/HW8.tsx
@@ -54,9 +54,9 @@ const HW8 = () => {
     }
 
     return (
-        <div id={'hw3'}>
+        <div id={'hw3'} className={s2.hw}>
             <div className={s2.hwTitle}>Homework #8</div>
-            <div className={s2.hw}>
+            <div>
                 <div className={s.container}>
                     <div className={s.buttonsContainer}>
                         <SuperButton
--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,29 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => {
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            const compareFn = action.payload === 'up' ? (a: UserType, b: UserType) => {
+                if (a.name[0] < b.name[0]) {
+                    return -1
+                } else if (a.name[0] > b.name[0]) {
+                    return 1
+                }
+                return 0
+            } : (a: UserType, b: UserType) => {
+                if (a.name[0] > b.name[0]) {
+                    return -1
+                } else if (a.name[0] < b.name[0]) {
+                    return 1
+                }
+                return 0
+            }
+            return state.sort(compareFn)
         }
         case 'check': {
 
-            return state // need to fix
+            return state.filter(u => u.age >= action.payload)
         }
         default:
             return state
